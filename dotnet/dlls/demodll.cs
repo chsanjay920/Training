ccb5170f8fd49cceb99145a82e6e7a4213931594
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-12-06 20:10:28 +0530

    Add files via upload

---
+++ dotnet/dlls/demodll.cs
@@ -0,0 +1,17 @@
+namespace nm{
+	class  Math
+	{
+		public int Sum(int x,int y)
+		{
+			return x+y;
+		}
+	}
+	namespace nm2{
+		class Math2{
+			public int Subtract(int x,int y)
+			{
+				return x-y;
+			}
+		}
+	}
+}
