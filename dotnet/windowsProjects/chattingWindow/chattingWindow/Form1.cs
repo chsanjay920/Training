febe9e74ebfa46eb097e572049bfbbdd316f0a5d
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-12-16 20:41:16 +0530

    Add files via upload

---
+++ dotnet/windowsProjects/chattingWindow/chattingWindow/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chattingWindow
+{
+    public delegate void Delegate(String str);
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Tbtext = textBox1.Text;
+            frm.OnTextChanged += new Delegate(txtChanged);
+            frm.Show();
+        }
+        private void txtChanged(String str)
+        {
+            textBox1.Text = str;
+        }
+    }
+}
