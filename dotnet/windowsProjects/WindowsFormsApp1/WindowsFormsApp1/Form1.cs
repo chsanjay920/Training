2773d85d270dac39a4261a5282de9c6c5653ed83
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-12-16 20:46:35 +0530

    Add files via upload

---
+++ dotnet/windowsProjects/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int j = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                for(int i = 0; i < openFileDialog.FileNames.Length; i++)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Location=new Point(j*100,j*100);
+                    pictureBox.SizeMode=PictureBoxSizeMode.StretchImage;
+                    pictureBox.Image = Image.FromFile(openFileDialog.FileNames[i]);
+                    
+                }
+            }
+        }
+    }
+}
