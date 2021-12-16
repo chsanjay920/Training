a9ac7868cbb2594f3ad14109faa61efff65229e1
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-12-16 20:43:55 +0530

    Add files via upload

---
+++ dotnet/windowsProjects/fiveButtonProgram/fiveButtonProgram/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace fiveButtonProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            int count = 1;
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5;j++)
+                {
+                    this.button1 = new System.Windows.Forms.Button();
+                    this.SuspendLayout();
+                    // 
+                    // button1
+                    // 
+                    this.button1.Location = new System.Drawing.Point(100*i, 50*j);
+                    this.button1.Name = "button1";
+                    this.button1.Size = new System.Drawing.Size(75, 23);
+                    this.button1.TabIndex = 0;
+                    this.button1.Text = count.ToString();
+                    this.button1.UseVisualStyleBackColor = true;
+                    this.button1.Click += new System.EventHandler(this.button1_Click);
+                    // 
+                    // Form1
+                    // 
+                    this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+                    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                    this.ClientSize = new System.Drawing.Size(615, 299);
+                    this.Controls.Add(this.button1);
+                    this.Name = "Form1";
+                    this.Text = "Form1";
+                    count++;
+                    this.ResumeLayout(false);
+                }
+            }
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ dotnet/windowsProjects/listBoxPractiseApplication/listBoxPractiseApplication/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listBoxPractiseApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(listBox1.SelectedItem.ToString());
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(listBox2.SelectedItem.ToString());
+            listBox2.Items.Remove(listBox2.SelectedItem);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*foreach(object item in listBox1.Items)
+            {
+                listBox2.Items.Add((string)item);
+            }
+            listBox1.Items.Clear();
+            */
+            int len = listBox1.Items.Count;
+            for(int i = 0;i<len; i++)
+            {
+                listBox2.Items.Add(listBox1.Items[0].ToString());
+                listBox1.Items.Remove(listBox1.Items[0]);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            /*foreach (object item in listBox2.Items)
+            {
+                listBox1.Items.Add((string)item);
+            }
+            listBox2.Items.Clear();
+            */
+            int len = listBox2.Items.Count;
+            for (int i = 0; i<len; i++)
+            {
+                listBox1.Items.Add(listBox2.Items[0].ToString());
+                listBox2.Items.Remove(listBox2.Items[0]);
+            }
+        }
+    }
+}
